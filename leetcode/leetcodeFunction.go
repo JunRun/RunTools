ef53162d5dededa0a89f7c043f060245fcb42058
JUNRUN <934776347@qq.com>
2019-12-17 14:59:15 +0800

    leetcode 二分查找题

--- leetcode/leetcodeFunction.go
+++ leetcode/leetcodeFunction.go
@@ -6,7 +6,9 @@
  */
 package leetcode
 
-import "strings"
+import (
+	"strings"
+)
 
 /**
 leetCode 解题 找出没有重复字符的最长子串
@@ -25,4 +27,63 @@ func LengthOfString(s string) int {
 
 	}
 	return length
+}
+
+/**给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+如果数组中不存在目标值，返回 [-1, -1]。
+*/
+func SearchRange(nums []int, target int) []int {
+	var i []int
+	start, end := -1, -1
+	left, right := 0, len(nums)-1
+	middle := 0
+
+	for l := 0; l < len(nums); l++ {
+
+		if left > right {
+			break
+		}
+		//middle = int(math.Floor(float64((left + right) / 2)))
+		middle = (left + right) / 2
+		if nums[middle] > target {
+			right = middle - 1
+		} else if nums[middle] < target {
+			left = middle + 1
+		} else if nums[middle] == target {
+			break
+		}
+	}
+	if left > right {
+		return append(i, -1, -1)
+	}
+	start = middle
+	for {
+		start--
+		if start < 0 {
+			start++
+			break
+		}
+		if nums[start] != target {
+			start++
+			break
+		}
+
+	}
+	end = middle
+	for {
+		end++
+		if end > len(nums)-1 {
+			end--
+			break
+		}
+		if nums[end] != target {
+			end--
+			break
+		}
+	}
+	i = append(i, start, end)
+	return i
 }
---
+++ test/leetcode_test.go
@@ -0,0 +1,19 @@
+/**
+ *
+ * @Description:
+ * @Version: 1.0.0
+ * @Date: 2019-12-17 14:29
+ */
+package test
+
+import (
+	"fmt"
+	"github.com/JunRun/RunTools/leetcode"
+	"testing"
+)
+
+func TestSearchRange(t *testing.T) {
+	var nums = []int{1}
+	fmt.Println(leetcode.SearchRange(nums, 1))
+
+}
