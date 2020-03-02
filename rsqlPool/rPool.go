709c19af1ad507633de8d574f526685c4a06b598
JUNRUN <934776347@qq.com>
2020-03-02 16:30:45 +0800

    实现简单的连接池

---
+++ rsqlPool/rPool.go
@@ -0,0 +1,62 @@
+/**
+ *
+ * @Description: 连接池
+ * @Version: 1.0.0
+ * @Date: 2020-03-02 15:33
+ */
+package rsqlPool
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"sync"
+)
+
+type Pool struct {
+	Lock    sync.Mutex
+	Res     chan io.Closer
+	Factory func() (io.Closer, error)
+	Closed  bool
+}
+
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("size 值过小")
+	}
+	return &Pool{
+		Res:     make(chan io.Closer, size),
+		Factory: fn,
+	}, nil
+}
+
+//获取连接
+func (p *Pool) Get() (io.Closer, error) {
+	select {
+	case r, ok := <-p.Res:
+		if !ok {
+			return nil, errors.New("连接池关闭")
+		}
+		return r, nil
+	default:
+		fmt.Println("创建资源")
+		return p.Factory()
+	}
+}
+
+//关闭连接池
+func (p *Pool) Close() {
+	p.Lock.Lock()
+	defer p.Lock.Unlock()
+
+	if p.Closed {
+		return
+	}
+	p.Closed = true
+	//关闭通道
+	close(p.Res)
+	//关闭资源
+	for r := range p.Res {
+		r.Close()
+	}
+}
